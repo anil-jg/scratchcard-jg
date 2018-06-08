253a409b5f4e1f66826a6b6c80d5a3464656d8e3
Anil Kumar Nayak <anil.k.nayak8@gmail.com>
2018-06-08 14:33:35 +0530

    added sound

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,5 @@
 import * as PIXI from "pixi.js";
+import * as PIXISound from "pixi-sound";
 
 import { AnimatedCoins } from "./animatedcoins";
 import { SimpleButton } from "./button";
@@ -14,7 +15,7 @@ export class Game extends PIXI.Application {
     private coins: AnimatedCoins;
 
     constructor() {
-        super(450, 450, {backgroundColor: 0xffffff, legacy: true});
+        super(450, 530, {backgroundColor: 0xffffff, legacy: true});
         document.body.appendChild(this.view);
 
         // set stage
@@ -28,6 +29,7 @@ export class Game extends PIXI.Application {
         PIXI.loader.add("assets/images/bg.jpg");
         PIXI.loader.add("assets/images/brush.png");
         PIXI.loader.add("assets/images/coin.json");
+        PIXISound.sound.add('afterplay', 'assets/sounds/aftergame.mp3');
 
         if (window.devicePixelRatio < 2) {
             PIXI.loader.add("assets/images/tiles.json");
@@ -58,11 +60,17 @@ export class Game extends PIXI.Application {
         }
 
         // add button
-        this.btnPlay = new SimpleButton(442, 50, 0xc3dc41);
-        this.btnPlay.position.set(500, 480);
+        this.btnPlay = new SimpleButton(300, 50, 0x261c0e);
+        this.btnPlay.position.set(80, 470);
         this.btnPlay.on("pointerdown", this.onPointerDown, this);
-        // this.stage.addChild(this.btnPlay);
+        this.stage.addChild(this.btnPlay);
 
+        // text message
+        this.textMessage = new PIXI.Text('Congrats You Won !!!',{fontFamily : 'Arial', fontSize: 30, fill : 0xFFD700, align : 'center'});
+        this.textMessage.position.x = 100;
+        this.textMessage.position.y = 300;
+        this.textMessage.alpha = 0;
+        this.stage.addChild(this.textMessage);
         // add animated coins
         this.coins = new AnimatedCoins(480);
         this.stage.addChild(this.coins);
@@ -79,6 +87,11 @@ export class Game extends PIXI.Application {
         const len: number = PRIZES.length;
         const values: number[] = [];
 
+        // this.textMessage.alpha = 0;
+        if(PIXISound.sound){
+            PIXISound.sound.stop("afterplay");    
+        }
+        this.textMessage.alpha = 0;
         // fill the value array with zeros
         for (let i = 0; i < len; i++) {
             values.push(0);
@@ -115,13 +128,13 @@ export class Game extends PIXI.Application {
 
     private resize(): void {
         // get the min ratio
-        const ratio = Math.min(window.innerWidth / 450, window.innerHeight / 450);
+        const ratio = Math.min(window.innerWidth / 450, window.innerHeight / 530);
 
         // scale stage
         this.stage.scale.set(ratio, ratio);
 
         // resize render
-        this.renderer.resize(450 * ratio, 450 * ratio);
+        this.renderer.resize(450 * ratio, 530 * ratio);
     }
 
     private onPointerDown(event: PIXI.interaction.InteractionEvent): void {
@@ -135,21 +148,45 @@ export class Game extends PIXI.Application {
 
     private onTileRevealed(tile: Tile): void {
         if (!this.isThereTileRemaining) {
-            this.btnPlay.text = "Play next scratchcard";
+            this.btnPlay.text = "Play Again";
             this.showResult();
         }
     }
 
     private showResult(): void {
+        // this.stage.addChild(this.textMessage);
+        var won = false;
+        var winningSet = false;
+        this.textMessage.alpha = 1;
+        var winCount = 0;
+        console.log(this.currentPrize);
         if (this.currentPrize > -1) {
             for (const tile of this.tiles) {
+                console.log(tile.prizeId);
                 if (tile.prizeId === this.currentPrize) {
+                    winCount++;
                     tile.win();
                     this.coins.play();
                 } else {
+                    // if(!winningSet){
+                    //     winningSet = true;
+                    //     won = false;    
+                    // }
                     tile.fail();
                 }
             }
+            if(winCount==3){
+                PIXISound.sound.play("afterplay")
+                this.textMessage.text = "Congrats You Won !!!";
+            }
+            else
+            {
+                this.textMessage.text = "Better luck next time !";   
+            }
+        }
+        else
+        {
+            this.textMessage.text = "Better luck next time !";   
         }
     }
 
